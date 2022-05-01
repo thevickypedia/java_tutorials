a24cb035f5a98edfee9ca54397caf1d6e7553462
thevickypedia <vignesh.sivanandarao@gmail.com>
2022-04-30 20:18:09 -0500

    Onboard a first class file to play with datetime Update gitignore

---
+++ src/datetime.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+public class datetime {
+    static LocalDateTime getDate() {
+        return LocalDateTime.now();
+    }
+    static DateTimeFormatter getFormat() {
+        return DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm:ss");
+    }
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = getFormat();
+        LocalDateTime now = getDate();
+        System.out.println("Current time: " + dtf.format(now));
+        LocalDate date_300_earlier = LocalDate.now().plusYears(1);
+        System.out.println("Date before a year: " + date_300_earlier);
+        LocalDate date_300_later = LocalDate.now().minusYears(1);
+        System.out.println("Date after a year: " + date_300_later);
+    }
+}
