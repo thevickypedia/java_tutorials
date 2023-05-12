9b4cf4a55834533f9766c9c9cf3494db324c8995
thevickypedia <vignesh.sivanandarao@gmail.com>
2023-05-12 15:34:21 -0500

    Use OS agnostic file paths

--- src/read_file_operations.java
+++ src/read_file_operations.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.util.Scanner;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class read_file_operations {
     static File check_file_exists(String filename, String... args) {
@@ -15,10 +17,12 @@ public class read_file_operations {
             System.out.println("File does not exist. Checking in " + System.getProperty("user.dir"));
         }
 
-        var file_path2 = System.getProperty("user.dir") + "/src/" + filename;
+        Path current_path = Paths.get(System.getProperty("user.dir"));
+        Path file_path = Paths.get(current_path.toString(), "src", filename);
+        var file_path2 = file_path.toString();
         File new_file = new File(file_path2);
         if (new_file.exists() && !file.isDirectory()) {
-            System.out.println("File `" + new_file.getName() + "` exists at " + System.getProperty("user.dir"));
+            System.out.println("File `" + new_file.getName() + "` exists at " + current_path);
             return new_file;
         } else {
             System.out.println("File does not exist.");
@@ -27,12 +31,14 @@ public class read_file_operations {
     }
 
     public static void read_file(File filename) throws Exception {
+        System.out.println("Reading with delimiter.");
         Scanner sc = new Scanner(filename);
         sc.useDelimiter("\\Z");
         System.out.println(sc.next());
     }
 
     public static void read_file_chunks(File filename) throws Exception {
+        System.out.println("Reading chunks.");
         Scanner sc_ = new Scanner(filename);
         while (sc_.hasNextLine())
             System.out.println(sc_.nextLine());
@@ -44,9 +50,8 @@ public class read_file_operations {
         if (file != null) {
             System.out.println();
             read_file(file);
+            System.out.println();
             read_file_chunks(file);
-        } else {
-            System.exit(1);
         }
     }
 }
